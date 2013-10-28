672084ed480ba276afb166cacd5ad775397ebfca
Ayrat-Salavatovich <ayrat.salavatovich@gmail.com>
2013-10-29 00:03:04 +0600

    Простое переключение на другой экран

---
+++ Day 4/RuslanAndLudmila/src/ayrat/salavatovich/gmail/com/day_4/ruslanandludmila/AboutActivity.java
@@ -0,0 +1,14 @@
+package ayrat.salavatovich.gmail.com.day_4.ruslanandludmila;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AboutActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.about);
+	}
+
+}
