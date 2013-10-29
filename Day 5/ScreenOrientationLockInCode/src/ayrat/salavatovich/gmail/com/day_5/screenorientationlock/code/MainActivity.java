6566f4a84d31c770c001e8108866b49fc37a0daa
Ayrat-Salavatovich <ayrat.salavatovich@gmail.com>
2013-10-29 23:36:32 +0600

    Установка ориентации программно

---
+++ Day 5/ScreenOrientationLockInCode/src/ayrat/salavatovich/gmail/com/day_5/screenorientationlock/code/MainActivity.java
@@ -0,0 +1,25 @@
+package ayrat.salavatovich.gmail.com.day_5.screenorientationlock.code;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.pm.ActivityInfo;
+import android.view.Menu;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+
+		setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+}
