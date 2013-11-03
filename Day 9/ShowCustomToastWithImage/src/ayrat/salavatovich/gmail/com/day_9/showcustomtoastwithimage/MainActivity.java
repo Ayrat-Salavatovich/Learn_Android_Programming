c6353c4435e7c93129669655ce8269f6f30a890b
Ayrat-Salavatovich <ayrat.salavatovich@gmail.com>
2013-11-03 21:40:29 +0600

    Создание собственных всплывающих уведомлений

---
+++ Day 9/ShowCustomToastWithImage/src/ayrat/salavatovich/gmail/com/day_9/showcustomtoastwithimage/MainActivity.java
@@ -0,0 +1,38 @@
+package ayrat.salavatovich.gmail.com.day_9.showcustomtoastwithimage;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	public void showToast(View v) {
+		LayoutInflater inflater = getLayoutInflater();
+		View layout = inflater.inflate(R.layout.custom_layout,
+				(ViewGroup) findViewById(R.id.toastLinearLayout));
+		Toast toast = new Toast(getApplicationContext());
+		toast.setGravity(Gravity.CENTER, 0, 0);
+		toast.setDuration(Toast.LENGTH_LONG);
+		toast.setView(layout);
+		toast.show();
+	}
+
+}
