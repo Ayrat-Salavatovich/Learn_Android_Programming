6f8558ec874020ce45627d34a65512fd3b2c46fd
Ayrat-Salavatovich <Ayrat.Salavatovich@gmail.com>
2014-09-01 20:55:33 +0600

    Game for Dilya

---
+++ Day 182/GameForDilya/src/ayrat/salavatovich/analyst/mx/day_182/gamefordilya/MainActivity.java
@@ -0,0 +1,15 @@
+package ayrat.salavatovich.analyst.mx.day_182.gamefordilya;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
